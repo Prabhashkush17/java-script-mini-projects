0256ee77045db829dbf41b8cba65c546ffdb243a
Prabhash kumar <prabhashkush17@gmail.com>
2025-07-08 13:33:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 
-function rollDice() {
-  const die1 = Math.floor(Math.random() * 6) + 1;
-  const die2 = Math.floor(Math.random() * 6) + 1;
 
-  document.getElementById("die1").src = `dice-img${die1}.png`;
-  document.getElementById("die2").src = `dice-img${die2}.png`;
+let principal = document.querySelector('#loanamtinput');
+let interest = document.querySelector('#intrestrateinput');
+let timetopay = document.querySelector('#timeintervalinput');
+let calculate = document.querySelector('#Calculate');
+let emi = document.querySelector('#emi');
 
-  document.getElementById("result").textContent = `Sum: ${die1 + die2}`;
+console.log(principal, interest, timetopay);
+
+function calculateEMI(){
+    if(principal.value === '' || interest.value === '' || timetopay.value === ''){
+        alert('Please Enter all the values');
+        return;
+    }
+    else{
+        let p = principal.value;
+        let r = interest.value / 1200;
+        let n = timetopay.value;
+        let emiValue = (p * r * (1+r) ** n) / ((1+r) ** n - 1);
+        emi.textContent = emiValue.toFixed(2);
+    }
 }
+
+calculate.addEventListener('click', calculateEMI);
